c918a360feb26c2d1da8c9753248b0682f0979a8
Fanfan durand <durand90@live.com>
2023-06-03 18:31:10 -0400

    cdf

--- next.js
+++ next.js
@@ -1,35 +1,36 @@
 
 
-function nonMatchChar(str) {
-    let letter = {}
-    let time = [];
+// function nonMatchChar(str) {
+//     let letter = {}
+//     let time = [];
 
-    for (i of str) {
-        if (letter[i]) {
-            letter[i] += 1;
-        } else {
-            letter[i] = 1;
-        }
-    } 
-    console.log(letter)
+//     for (i of str) {
+//         if (letter[i]) {
+//             letter[i] += 1;
+//         } else {
+//             letter[i] = 1;
+//         }
+//     } 
+//     console.log(letter)
     
-    for (const key in letter) {
-    if (Object.values(letter) == 1) { 
-        console.log(Object.keys(letter))
-    } 
-    }
-    console.log(time)
-    let y = Object.values(letter)
+//     let p = Object.values(letter)
+
+
+//     console.log(p)
+//     let y = Object.values(letter)
     
-    for (i of y) {
-        if (i > 1) {
-            console.log('greater')
-        } else {
-            console.log('lesser')
-        }
-    }
+//     for (i of y) {
+//         if (i > 1) {
+//             console.log('greater')
+//         } else {
+//             console.log('lesser')
+//         }
+//     }
     
 
-}
+// }
 
-console.log(nonMatchChar('a very funny string'))
+// console.log(nonMatchChar('a very funny string'))
+
+
+function test()
