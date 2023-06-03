a2bf266d8eca10778a4301b27581d65e370eec4a
Fanfan durand <durand90@live.com>
2023-06-03 19:30:08 -0400

    kjl

---
+++ findCount.js
@@ -0,0 +1,16 @@
+
+
+function findCount(str) {
+
+    let paper = {};
+
+    for (i of str) {
+        if (paper[i]) {
+            paper[i] += 1;
+        } else if (!paper[i]) {
+            paper[i] = 1;
+        }
+    } return paper;
+}
+
+console.log(findCount('str about this ok for'))
