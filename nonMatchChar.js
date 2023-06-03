f8ac32857f2109d08cc2fc01c043e714ff4c26ef
Fanfan durand <durand90@live.com>
2023-06-03 19:08:37 -0400

    asd

---
+++ nonMatchChar.js
@@ -0,0 +1,35 @@
+
+
+function nonMatchChar(str) {
+    let letter = {}
+    let p = [];
+
+    for (i of str) {
+        if (letter[i]) {
+            letter[i] += 1;
+        } else {
+            letter[i] = 1;
+        }
+    } 
+    console.log(letter)
+
+    for (let key in letter){
+    // console.log(`${letter[key]}`);
+    
+    if (letter[key] > 2) {
+        p.push(key)
+        console.log(p)
+    }
+
+        
+    }
+    
+
+    
+} 
+    
+
+
+
+console.log(nonMatchChar('a very funny string'))
+
