d827f8303c83d50918835e76d9559d1c58e27b01
Fanfan durand <durand90@live.com>
2023-06-12 09:40:57 -0400

    plk

---
+++ test.js
@@ -0,0 +1,62 @@
+// function solution(n) {
+//     let y = 1;
+//     let x = 0;
+//     for (i of n) {
+        
+//         y = y * n[i];
+//         x = x + n[i];
+//     }
+//     console.log(y)
+//     console.log(x)
+//     let m = y - x;
+      
+//    return m;
+
+// } 
+
+
+
+
+// console.log(solution(1010))
+
+// let a = 1 || 2 || 3 || 4 || 5; 
+
+// console.log(a);
+
+
+// function apb() {
+// if (a == 2) {
+//     return true
+// } else {
+//     return false
+// }
+// }
+
+// console.log()
+
+
+
+
+
+// let number = 345;
+// let b = String(number).split('')
+// console.log(b)
+
+// let str = number.toString();
+// let answer = 0;
+
+// str.split('')
+// console.log(str)
+// for (let i in str) answer += Math.pow(str[i], Number(i)+1);
+
+
+
+function test(n) {
+if (isArray(n)) {
+    
+    return "yes"
+}
+
+}
+
+console.log(test[1,2,3,4])cfd
